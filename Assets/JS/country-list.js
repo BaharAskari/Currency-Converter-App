207f2f104cffca12da1cf4e948479a6e446c6b9f
BaharAskari <bahar.askari97@gmail.com>
2022-03-27 00:07:20 +0430

    currency converter app

---
+++ Assets/JS/country-list.js
@@ -0,0 +1,230 @@
+let country_list = {
+    "AED" : "AE",
+    "AFN" : "AF",
+    "XCD" : "AG",
+    "ALL" : "AL",
+    "AMD" : "AM",
+    "ANG" : "AN",
+    "AOA" : "AO",
+    "AQD" : "AQ",
+    "ARS" : "AR",
+    "AUD" : "AU",
+    "AZN" : "AZ",
+    "BAM" : "BA",
+    "BBD" : "BB",
+    "BDT" : "BD",
+    "XOF" : "BE",
+    "BGN" : "BG",
+    "BHD" : "BH",
+    "BIF" : "BI",
+    "BMD" : "BM",
+    "BND" : "BN",
+    "BOB" : "BO",
+    "BRL" : "BR",
+    "BSD" : "BS",
+    "NOK" : "BV",
+    "BWP" : "BW",
+    "BYR" : "BY",
+    "BZD" : "BZ",
+    "CAD" : "CA",
+    "CDF" : "CD",
+    "XAF" : "CF",
+    "CHF" : "CH",
+    "CLP" : "CL",
+    "CNY" : "CN",
+    "COP" : "CO",
+    "CRC" : "CR",
+    "CUP" : "CU",
+    "CVE" : "CV",
+    "CYP" : "CY",
+    "CZK" : "CZ",
+    "DJF" : "DJ",
+    "DKK" : "DK",
+    "DOP" : "DO",
+    "DZD" : "DZ",
+    "ECS" : "EC",
+    "EEK" : "EE",
+    "EGP" : "EG",
+    "ETB" : "ET",
+    "EUR" : "FR",
+    "FJD" : "FJ",
+    "FKP" : "FK",
+    "GBP" : "GB",
+    "GEL" : "GE",
+    "GGP" : "GG",
+    "GHS" : "GH",
+    "GIP" : "GI",
+    "GMD" : "GM",
+    "GNF" : "GN",
+    "GTQ" : "GT",
+    "GYD" : "GY",
+    "HKD" : "HK",
+    "HNL" : "HN",
+    "HRK" : "HR",
+    "HTG" : "HT",
+    "HUF" : "HU",
+    "IDR" : "ID",
+    "ILS" : "IL",
+    "INR" : "IN",
+    "IQD" : "IQ",
+    "IRR" : "IR",
+    "ISK" : "IS",
+    "JMD" : "JM",
+    "JOD" : "JO",
+    "JPY" : "JP",
+    "KES" : "KE",
+    "KGS" : "KG",
+    "KHR" : "KH",
+    "KMF" : "KM",
+    "KPW" : "KP",
+    "KRW" : "KR",
+    "KWD" : "KW",
+    "KYD" : "KY",
+    "KZT" : "KZ",
+    "LAK" : "LA",
+    "LBP" : "LB",
+    "LKR" : "LK",
+    "LRD" : "LR",
+    "LSL" : "LS",
+    "LTL" : "LT",
+    "LVL" : "LV",
+    "LYD" : "LY",
+    "MAD" : "MA",
+    "MDL" : "MD",
+    "MGA" : "MG",
+    "MKD" : "MK",
+    "MMK" : "MM",
+    "MNT" : "MN",
+    "MOP" : "MO",
+    "MRO" : "MR",
+    "MTL" : "MT",
+    "MUR" : "MU",
+    "MVR" : "MV",
+    "MWK" : "MW",
+    "MXN" : "MX",
+    "MYR" : "MY",
+    "MZN" : "MZ",
+    "NAD" : "NA",
+    "XPF" : "NC",
+    "NGN" : "NG",
+    "NIO" : "NI",
+    "NPR" : "NP",
+    "NZD" : "NZ",
+    "OMR" : "OM",
+    "PAB" : "PA",
+    "PEN" : "PE",
+    "PGK" : "PG",
+    "PHP" : "PH",
+    "PKR" : "PK",
+    "PLN" : "PL",
+    "PYG" : "PY",
+    "QAR" : "QA",
+    "RON" : "RO",
+    "RSD" : "RS",
+    "RUB" : "RU",
+    "RWF" : "RW",
+    "SAR" : "SA",
+    "SBD" : "SB",
+    "SCR" : "SC",
+    "SDG" : "SD",
+    "SEK" : "SE",
+    "SGD" : "SG",
+    "SKK" : "SK",
+    "SLL" : "SL",
+    "SOS" : "SO",
+    "SRD" : "SR",
+    "STD" : "ST",
+    "SVC" : "SV",
+    "SYP" : "SY",
+    "SZL" : "SZ",
+    "THB" : "TH",
+    "TJS" : "TJ",
+    "TMT" : "TM",
+    "TND" : "TN",
+    "TOP" : "TO",
+    "TRY" : "TR",
+    "TTD" : "TT",
+    "TWD" : "TW",
+    "TZS" : "TZ",
+    "UAH" : "UA",
+    "UGX" : "UG",
+    "USD" : "US",
+    "UYU" : "UY",
+    "UZS" : "UZ",
+    "VEF" : "VE",
+    "VND" : "VN",
+    "VUV" : "VU",
+    "YER" : "YE",
+    "ZAR" : "ZA",
+    "ZMK" : "ZM",
+    "ZWD" : "ZW"
+}
+
+
+const dropList = document.querySelectorAll(".drop-list select"),
+fromCurrency = document.querySelector(".from select"),
+toCurrency = document.querySelector(".to select"),
+getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++){
+    for(currency_code in country_list){  
+      let selected;
+      if(i == 0){
+        selected = currency_code == "USD" ? "selected" : "";
+      }else if(i == 1){
+        selected = currency_code == "IRR" ? "selected" : "";
+      }
+      let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+      dropList[i].insertAdjacentHTML("beforeend",optionTag);
+    }
+     dropList[i].addEventListener("change", e =>{
+       loadFlag(e.target);
+     });
+}
+
+function loadFlag(element){
+  for(code in country_list){
+    if(code == element.value){
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://www.countryflags.io/${country_list[code]}/flat/64.png`;
+    }
+  }
+}
+
+window.addEventListener("load" , () =>{
+  getExchangeRate();
+});
+
+getButton.addEventListener("click" , e =>{
+  e.preventDefault(); // preventing form from submitting
+  getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", ()=>{
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
+});
+
+function getExchangeRate(){
+  const amount = document.querySelector(".amount input"),
+  exchangeRateTxt = document.querySelector(".exchange-rate");
+  let amountVal = amount.value;
+  if (amountVal == "" || amountVal == "0"){
+    amount.value = "1";
+    amountVal = 1;
+  }
+  exchangeRateTxt.innerHTML ="Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/f1c69b49c0f17050c420ce96/latest/${fromCurrency.value}`;
+  fetch(url).then(response => response.json()).then(result => {
+    let exchangeRate = result.conversion_rates[toCurrency.value];
+    let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+    exchangeRateTxt.innerHTML = `${amountVal} ${fromCurrency.value}  = ${totalExchangeRate} ${toCurrency.value}`;
+  }).catch(() => {
+      exchangeRateTxt.innerHTML = "Something went wrong!"
+  })
+}
---
+++ Assets/JS/script.js
@@ -0,0 +1,31 @@
+// const dropList = document.querySelectorAll(".drop-list select");
+
+// for (let i = 0; i < dropList.length; i++){
+//     for(currency_code in country_list){  
+//       console.log(code);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
